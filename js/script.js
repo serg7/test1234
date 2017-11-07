05073cafc8875e882efea104e1e9f3b255a51f6f
Sergey <serhiyborsuk@gmail.com>
2017-11-07 23:44:59 +0200

    add files

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+
+// $('.img').hover(
+//     function(){
+//         $(this).addClass('img-overlay')
+//     },
+//     function(){
+//         $(this).removeClass('img-overlay')
+//     }
+// )
